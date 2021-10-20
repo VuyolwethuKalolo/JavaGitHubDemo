ffb1a6bf370cf39e2ac027559c29d6b57dd18fb5
Vuyolwethu Kalolo <Vuyolwethukalolo1994@gmail.com>
2021-10-21 00:41:02 +0200

    My first commit

---
+++ JavaGitHubDemo/src/Addition.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Master Vee
+ */
+public class Addition {
+      /**
+     * This adds two real numbers 
+     * @param a
+     * @param b
+     * @return 
+     */
+     public static double  doubleSum(double a, double b){
+     return a + b;
+     }
+    
+}
